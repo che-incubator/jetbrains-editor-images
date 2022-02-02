ac725a4dd370395d1b0a8944f467dc49744008aa
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2022-02-02 22:08:14 +0200

    Provide user to passwd and group file
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

--- static/entrypoint-volume.sh
+++ static/entrypoint-volume.sh
@@ -8,6 +8,14 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
+# Add current (arbitrary) user to /etc/passwd and /etc/group
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
+  fi
+fi
+
 # necessary environment variable: PROJECTOR_ASSEMBLY_DIR
 if [ -n "$PROJECTOR_ASSEMBLY_DIR" ]; then
   cd "$PROJECTOR_ASSEMBLY_DIR"/ide/bin || exit
